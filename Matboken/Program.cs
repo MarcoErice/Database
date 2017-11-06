e73453a5e9b8902db4932664defa87d0b3918aa8
MarcoErice <marco.erice@gmail.com>
2017-11-06 13:52:58 +0100

    My first database exercise.

--- Matboken/Program.cs
+++ Matboken/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,16 @@ namespace Matboken
     {
         static void Main(string[] args)
         {
+            SQLiteConnection cn = new SQLiteConnection(@"Data Source=C:\Users\marco\Downloads\sqlitestudio-3.1.1\SQLiteStudio\Matboken");
+            cn.Open();
+            var cmd = cn.CreateCommand();
+            cmd.CommandText = "SELECT name FROM Ingredienser";
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader.GetString(0));
+            }
+            cn.Close();
         }
     }
 }
